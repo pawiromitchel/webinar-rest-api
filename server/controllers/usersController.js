438afccc5d976fb0a203b4fdd4d290cd704bb628
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2018-12-23 23:18:31 -0300

    implemented ORM

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,91 +1,57 @@
-// const mysql = require('mysql');
 const databaseConfig = require('../config/config');
-// const connection = mysql.createConnection(databaseConfig[process.env.ENVIRONMENT]);
 
 const Sequelize = require('sequelize');
-const connection = new Sequelize({
-    host: 'localhost',
-    username : 'root',
-    password: '',
-    database: 'rest-api',
-    dialect: 'mysql',
-    define: {
-        timestamps: false
-    }
-});
+const connection = new Sequelize(databaseConfig[process.env.ENVIRONMENT || 'development']);
 
 const users = connection.define('users', {
     user_id: {
         type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+        primaryKey: true
     },
     first_name: Sequelize.STRING,
     last_name: Sequelize.STRING,
     created_at: Sequelize.DATE,
-});
+}, {
+        freezeTableName: true
+    });
 
 function create(req, res) {
-    // connection.query('INSERT INTO users (first_name, last_name) VALUES(?,?)', [
-    //     req.body.first_name,
-    //     req.body.last_name
-    // ], function (error, results, fields) {
-    //     if (error) {
-    //         res.json({ message: error });
-    //     }
-
-    //     res.json({ message: `Inserted ID: ${results.insertId}` });
-    // });
+    users.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+    }).then(users => res.json(users)).catch(error => res.json({ message: error }));
 }
 
 function selectAll(req, res) {
-    // connection.query('SELECT * FROM users', function (error, results, fields) {
-    //     if (error) {
-    //         res.json({ message: error });
-    //     }
-
-    //     res.json(results);
-    // })
-
     users.findAll().then(users => res.json(users)).catch(error => res.json({ message: error }));
 }
 
 function selectOne(req, res) {
-    // connection.query('SELECT * FROM users WHERE user_id = ?', [
-    //     req.params.user_id
-    // ], function (error, results, fields) {
-    //     if (error) {
-    //         res.json({ message: error });
-    //     }
-
-    //     res.json(results);
-    // })
+    users.findOne({
+        where: {
+            user_id: req.params.user_id
+        }
+    }).then(users => res.json(users)).catch(error => res.json({ message: error }));
 }
 
 function update(req, res) {
-    // connection.query('UPDATE users SET first_name = ?, last_name = ? WHERE user_id = ?', [
-    //     req.body.first_name,
-    //     req.body.last_name,
-    //     req.body.user_id
-    // ], function (error, results, fields) {
-    //     if (error) {
-    //         res.json({ message: error });
-    //     }
-
-    //     res.json(results);
-    // });
+    users.update({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+    }, {
+            where: {
+                user_id: req.body.user_id
+            }
+        }
+    ).then(users => res.json(users)).catch(error => res.json({ message: error }));
 }
 
 function deleteOne(req, res) {
-    // connection.query('DELETE FROM users WHERE user_id = ?', [
-    //     req.body.user_id
-    // ], function (error, results, fields) {
-    //     if (error) {
-    //         res.json({ message: error });
-    //     }
-
-    //     res.json(results);
-    // });
+    users.destroy({
+        where: {
+            user_id: req.body.user_id
+        }
+    }).then(users => res.json(users)).catch(error => res.json({ message: error }));
 }
 
 module.exports = { create, selectAll, update, deleteOne, selectOne };
